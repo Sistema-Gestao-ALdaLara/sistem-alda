63d17803949c99a719022c363c244437d618771d
FlavioFj18 <fuxiflavio86@gmail.com>
2025-03-20 20:50:39 +0100

    upload

---
+++ includes/head.php
@@ -0,0 +1,39 @@
+<head>
+    <title>Admin</title>
+    <!-- HTML5 Shim and Respond.js IE10 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 10]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="#">
+    <meta name="keywords" content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="author" content="#">
+    <!-- Favicon icon -->
+    <link rel="icon" href="libraries\assets\images\favicon.ico" type="image/x-icon">
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="libraries\bower_components\bootstrap\css\bootstrap.min.css">
+    <!-- feather Awesome -->
+    <link rel="stylesheet" type="text/css" href="libraries\assets\icon\feather\css\feather.css">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="libraries\assets\css\style.css">
+    <link rel="stylesheet" type="text/css" href="libraries\assets\css\jquery.mCustomScrollbar.css">
+
+    <style>
+        .bg-img {
+          width: 100%; /* Ou um valor específico, como 500px */
+          height: 100%; /* Defina a altura conforme necessário */
+          background-image: url('assets/img/IMG_20250113_104838.jpg'); /* Caminho da imagem */
+          background-size: cover; /* Faz com que a imagem cubra toda a div */
+          background-position: center; /* Centraliza a imagem */
+          background-repeat: no-repeat; /* Evita repetições da imagem */
+        }
+      </style>
+      
+</head>
